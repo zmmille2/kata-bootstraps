b937774ca06f47ea0b70be060534c50a37866d49
Tanner Barlow <tanner.barlow.dev@gmail.com>
2021-01-22 11:13:20 -0700

    Ignore function file

--- ts/typescript-jest/src/week3/nextClosestTime.ts
+++ ts/typescript-jest/src/week3/nextClosestTime.ts
@@ -1,7 +1,8 @@
+/* eslint-disable */
 /**
  * Returns the next closest time using the same digits
  * provided in the input string
- * 
+ *
  * @param {string} time Time represented in format `HH:MM`
  * @returns {string} Next closest time using digits from `time`
  */
