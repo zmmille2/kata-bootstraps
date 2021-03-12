9e184171bea2a89d421282e8b215618de500f960
Tanner Barlow <tanner.barlow.dev@gmail.com>
2021-03-12 08:09:14 -0700

    WIP

--- ts/typescript-jest/src/storage/providers/azureBlobStorageService.test.ts
+++ ts/typescript-jest/src/storage/providers/azureBlobStorageService.test.ts
@@ -10,19 +10,25 @@ describe("Azure Blob Storage Service", () => {
 
   beforeEach(() => {
     // TODO - mock out downloadToBuffer and uploadData functions of BlockBlobClient
-
+    BlockBlobClient.prototype.downloadToBuffer = jest.fn(() =>
+      Promise.resolve(Buffer.from(mockBlobContent))
+    );
+    BlockBlobClient.prototype.uploadData = jest.fn();
     storage = new AzureBlobStorageService("mystorageaccount");
   });
 
   it("reads from Azure Blob Storage", async () => {
     const content = await storage.read("container/blob.txt");
-    // TODO - assert downloadToBuffer was called
-    // TODO - assert content is same as mockBlobContent
+    expect(BlockBlobClient.prototype.downloadToBuffer).toBeCalled();
+    expect(content).toBe(mockBlobContent);
+    // TODO - assert downloadToBuffer was called ✅
+    // TODO - assert content is same as mockBlobContent ✅
   });
 
   it("writes to Azure Blob Storage", async () => {
     const myBlobContent = "hello";
     await storage.write("container/blob.txt", myBlobContent);
+    expect(BlockBlobClient.prototype.downloadToBuffer).toBeCalled();
     // TODO - assert uploadData is called with myBlobContent
   });
 });
